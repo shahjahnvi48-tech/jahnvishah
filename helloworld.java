ab9971c0d2942f35957974400fa497c5a004dd35
JAHNVI SHAH <256796066+shahjahnvi48-tech@users.noreply.github.com>
2026-03-05 18:48:46 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -5,28 +5,5 @@ public class HelloWorld {
 // EXPECTED OUTPUT: Hello,World!
 
 
-// classes and objects 
-class Student {
-
-    int id;
-    String name;
-
-    void display() {
-        System.out.println(id);
-        System.out.println(name);
-    }
-
-    public static void main(String[] args) {
-
-        Student s1 = new Student();
-
-        s1.id = 101;
-        s1.name = "Jahnvi";
-
-        s1.display();
-    }
-}
-    // expected output: 101
-                     // jahnvi
     
                     
