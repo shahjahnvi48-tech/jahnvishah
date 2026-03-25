5c3b842d178b528d0725c47defcee3fd1ba83d19
JAHNVI SHAH <256796066+shahjahnvi48-tech@users.noreply.github.com>
2026-03-25 23:07:35 +0530

    Create Taking input in Java.java

---
+++ Taking input in Java.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        long c = sc.nextLong();
+        byte d = sc.nextByte();
+        sc.nextLine(); // consume newline
+        String s = sc.nextLine();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+    }
+}
